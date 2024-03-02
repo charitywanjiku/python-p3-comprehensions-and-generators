b578f2901bdc654bbb3d1623dfc334e3a6170ae2
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-03-02 11:43:38 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def return_evens(num_list):
-    pass
+    evens = []
+    for num in num_list:
+        if num % 2 == 0:  
+            evens.append(num)  
+    return evens    
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 
 def make_exclamation(sentence_list):
-    pass
+     exclamation = []
+     for sentence in sentence_list:
+        exclamation.append(sentence + "!") 
+     return exclamation
+
+    
